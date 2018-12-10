d1925dcaceb807b767d18dd08c3d65c144f3c8fc
John Kercheval <jkercheval@godaddy.com>
2018-12-10 14:52:49 -0800

    Day 8 part 1

--- day8.js
+++ day8.js
@@ -20,7 +20,6 @@ rawTreeData.forEach(function(element) {
 // Part 1
 
 function sumMetaData(state) {
-  let sum = 0;
   let nodeCount = state.treeData[state.index];
   let metaDataCount = treeData[state.index + 1];
   state.index += 2;
@@ -31,15 +30,60 @@ function sumMetaData(state) {
     state.sum += state.treeData[state.index];
     state.index++;
   }
+  return state;
 }
 
-let state = {
+let sumState = {
   treeData: treeData,
   index: 0,
   sum: 0
 };
-sumMetaData(state);
+sumMetaData(sumState);
 
-console.log('Metadata sum is', state.sum);
+console.log('Metadata sum is', sumState.sum);
+
+// Part 2
+function getLeafNodeValue(state) {
+  let metaDataSum = 0;
+  let nodeCount = state.treeData[state.index];
+  let metaDataCount = treeData[state.index + 1];
+  state.index += 2;
+  for (let i = 0; i < metaDataCount; i++) {
+    metaDataSum+= state.treeData[state.index];
+    state.index++;
+  }
+  return metaDataSum;
+}
+
+function getNodeValue(state) {
+  let nodeValue = 0;
+  let nodeCount = state.treeData[state.index];
+  if (!nodeCount) {
+    nodeValue += getLeafNodeValue(state);
+  } else {
+    let nodeValues = [];
+    let metaDataCount = treeData[state.index + 1];
+    state.index += 2;
+    for (let i = 0; i < nodeCount; i++) {
+      nodeValues.push(getNodeValue(state));
+    }
+    for (let i = 0; i < metaDataCount; i++) {
+      if (treeData[state.index] <= nodeValues.length) {
+        nodeValue += nodeValues[treeData[state.index] - 1];
+      }
+      state.index ++;
+    }
+  }
+  return nodeValue;
+}
+
+let nodeState = {
+  treeData: treeData,
+  index: 0
+};
+let nodeValue = getNodeValue(nodeState);
+
+console.log('Root node value is', nodeValue);
+
 
 console.log('END');
